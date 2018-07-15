518d2b28d1d2700e5c10a60e36f10c0a13645812
blckdrm <35443111+his9989@users.noreply.github.com>
2018-07-15 17:47:29 +0900

    Update back_1463_make_num_one.cpp

--- back_1463_make_num_one.cpp
+++ back_1463_make_num_one.cpp
@@ -1,31 +1,38 @@
-﻿
-Microsoft Visual Studio Solution File, Format Version 12.00
-# Visual Studio 15
-VisualStudioVersion = 15.0.27703.2035
-MinimumVisualStudioVersion = 10.0.40219.1
-Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "back_1463_make_num_one", "back_1463_make_num_one\back_1463_make_num_one.vcxproj", "{0F533768-3C01-4D42-B262-73DB294BBA09}"
-EndProject
-Global
-	GlobalSection(SolutionConfigurationPlatforms) = preSolution
-		Debug|x64 = Debug|x64
-		Debug|x86 = Debug|x86
-		Release|x64 = Release|x64
-		Release|x86 = Release|x86
-	EndGlobalSection
-	GlobalSection(ProjectConfigurationPlatforms) = postSolution
-		{0F533768-3C01-4D42-B262-73DB294BBA09}.Debug|x64.ActiveCfg = Debug|x64
-		{0F533768-3C01-4D42-B262-73DB294BBA09}.Debug|x64.Build.0 = Debug|x64
-		{0F533768-3C01-4D42-B262-73DB294BBA09}.Debug|x86.ActiveCfg = Debug|Win32
-		{0F533768-3C01-4D42-B262-73DB294BBA09}.Debug|x86.Build.0 = Debug|Win32
-		{0F533768-3C01-4D42-B262-73DB294BBA09}.Release|x64.ActiveCfg = Release|x64
-		{0F533768-3C01-4D42-B262-73DB294BBA09}.Release|x64.Build.0 = Release|x64
-		{0F533768-3C01-4D42-B262-73DB294BBA09}.Release|x86.ActiveCfg = Release|Win32
-		{0F533768-3C01-4D42-B262-73DB294BBA09}.Release|x86.Build.0 = Release|Win32
-	EndGlobalSection
-	GlobalSection(SolutionProperties) = preSolution
-		HideSolutionNode = FALSE
-	EndGlobalSection
-	GlobalSection(ExtensibilityGlobals) = postSolution
-		SolutionGuid = {8694007C-5524-432C-B3AE-150544D3BC3E}
-	EndGlobalSection
-EndGlobal
+#include<iostream>
+using namespace std;
+
+/*
+x에 대한 답을 구하기 위해서 1부터 차근차근 답을 구합니다.
+
+즉, 5를 구하고 싶은 경우에는 1, 2, 3, 4에 대한 답을 먼저 구합니다.
+그리고, 그 답을 이용하여 5에 대한 답을 구하게 됩니다.
+
+1부터 차근차근 구한 답은 배열의 원소로 저장한다.
+
+즉, x에 대한 답은 arr[x]에 저장하게 된다.
+*/
+
+int min(int a, int b) {							// a, b를 입력하면 작은 값을 반환하는 함수입니다.
+	if (a > b) return b;
+	else return a;
+}
+
+int main(void) {
+	int input, i;							// input : 1을 만들고자 하는 입력값
+	cin >> input;
+	int length = input + 2;						// 직관성을 위해서, x에 대한 답은 arr[x]에 저장합니다. 그래서 length=input+1;입니다.
+									// 동적 배열 크기에 의한 런타임 에러 발생으로, length의 크기를 하나 더 키웠습니다.
+
+	int *num = NULL;						// 크기가 length인 동적 배열 선언
+	num = new int[length];
+
+	num[1] = 0;							// 입력이 1인 경우, 답은 0입니다. (입력이 0인 경우는 무의미합니다.)
+	for (i = 2; i <= input; i++) {					// 2인 경우 부터, 구하고자 하는 입력값까지 연산을 반복하여 답을 구합니다.
+
+		num[i] = num[i - 1] + 1;				// num[i]에 1을 빼는 경우를 먼저 넣어둔다. 이건 어느 경우라도 비교해야하는 기본 값이므로
+		if (i % 2 == 0) num[i] = min(num[i], num[i / 2] + 1);	// 2의 배수인 경우에는, 2로 나누는 경우와 1을 빼는 경우 중 무엇이 더 효율적인지 비교한다.
+		if (i % 3 == 0) num[i] = min(num[i], num[i / 3] + 1);	// 3의 배수인 경우, num[i]에는 이미 2로 나누는 경우와 1을 빼는 경우 중의 작은 값이 있으므로, 이와 3으로 나눈 경우 중 최소값 비교
+	}
+	cout << num[input];						// input에 해당하는 답을 출력합니다.
+	return 0;
+}
