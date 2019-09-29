dfde138bc59c82966706d93bd92474892e17f442
InSoo <his9989@naver.com>
2019-09-29 18:24:30 +0900

    Add files via upload

---
+++ baekjoon/dfs_bfs/baekjoon_11403_경로 찾기.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+using namespace std;
+int N, map[100][100];
+
+// 경로 찾기
+int main() {
+	cin >> N;
+	for (int y = 0; y < N; y++) 
+		for (int x = 0; x < N; x++) 
+			cin >> map[y][x];
+
+	for (int k = 0; k < N; k++) {
+		for (int y = 0; y < N; y++) {
+			for (int x = 0; x < N; x++) {
+				if (map[y][k] == 1 && map[k][x] == 1) {
+					map[y][x] = 1;
+				}
+			}
+		}
+	}
+	for (int y = 0; y < N; y++) {
+		for (int x = 0; x < N; x++)
+			cout << map[y][x] << " ";
+		cout << endl;
+	}
+}
